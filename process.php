368adc8a1545f31800bfc74cba52aeef808e7bab
piotrpiekos <piotrpiekos@gmail.com>
2023-10-05 02:02:15 +0300

    attempt to fix email sending

--- process.php
+++ process.php
@@ -1,18 +1,14 @@
 <?php
-
-    $to = "mr3essa@gmail.com";
+    $to = "piotrpiekos@gmail.com";
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
     $headers = "From: $from";
-    $subject = "You have a message.";
+    $subject = "You have a message from piotrpiekos.github.io.";
 
     $fields = array();
     $fields{"name"} = "name";
     $fields{"email"} = "email";
-    $fields{"phone"} = "phone";
     $fields{"message"} = "message";
-
-    
 
     $body = "Here is what was sent:\r\n"; 
 
